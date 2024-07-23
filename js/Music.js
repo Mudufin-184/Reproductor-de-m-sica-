3aa9cb2647629a51658719b132d3947e0afc5ba8
YojanVela <yojanevela@gmail.com>
2024-07-23 16:32:12 -0500

    Añadir función de saludo dinámico basado en la hora del día

--- js/Music.js
+++ js/Music.js
@@ -1,15 +1,17 @@
-const greeting = document.getElementById('greeting');
+document.addEventListener("DOMContentLoaded", function() {
+    const greeting = document.getElementById('greeting');
 
-const updateGreeting = () => {
-    const now = new Date();
-    const hour = now.getHours();
-    if (hour < 12) {
-        greeting.textContent = 'Buenos Días ✨';
-    } else if (hour < 18) {
-        greeting.textContent = 'Buenas Tardes ✨';
-    } else {
-        greeting.textContent = 'Buenas Noches ✨';
-    }
-};
+    const updateGreeting = () => {
+        const now = new Date();
+        const hour = now.getHours();
+        if (hour < 12) {
+            greeting.textContent = 'Buenos Días ✨';
+        } else if (hour < 18) {
+            greeting.textContent = 'Buenas Tardes ✨';
+        } else {
+            greeting.textContent = 'Buenas Noches ✨';
+        }
+    };
 
-updateGreeting();
+    updateGreeting();
+});
