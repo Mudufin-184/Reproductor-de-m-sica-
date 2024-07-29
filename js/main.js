6c5316a3d0c88dd60dd19c274dc26eb60bf466dc
Mudufin-184 <velandiafonseca@gmail.com>
2024-07-29 16:49:19 -0500

    Nuevo orden de Canciones

--- js/main.js
+++ js/main.js
@@ -2,9 +2,39 @@ document.addEventListener('DOMContentLoaded', () => {
     const songs = [
         {
             artist: 'AC DC',
+            songName: 'Rock or Bust',
+            songPath: 'music/AC DC  Rock or Bust (Official Video).mp3',
+            imagePath: 'images/AC DC  Rock or Bust (Official Video).jpg'
+        },
+        {
+            artist: 'Billie Eilish',
+            songName: 'When The Party’s Over',
+            songPath: 'music/Billie Eilish when the party over (Traducida al Español).mp3',
+            imagePath: 'images/Billie Eilish  when the partys over (Traducida al Español).jpg'
+        },
+        {
+            artist: 'Benson Boone',
+            songName: 'Beautiful Things',
+            songPath: 'music/Benson Boone  Beautiful Things (Español  Lyrics)  video musical.mp3',
+            imagePath: 'images/Benson Boone Beautiful Things.jpg' // Asegúrate de tener esta imagen en la carpeta images
+        },
+        {
+            artist: 'Lewis Capaldi',
+            songName: 'Someone You Loved',
+            songPath: 'music/Someone You Loved  Lewis Capaldi Subtitulada Lyrics.mp3',
+            imagePath: 'images/Someone You Loved  Lewis Capaldi.jpg'
+        },
+        {
+            artist: 'AC DC',
             songName: 'Thunderstruck',
             songPath: 'music/AC DC  Thunderstruck (Official Video).mp3',
             imagePath: 'images/AC DC  Thunderstruck (Official Video).jpg'
+        },
+        {
+            artist: 'Ember Island',
+            songName: 'Umbrella',
+            songPath: 'music/Ember Island Umbrella (Español).mp3',
+            imagePath: 'images/maxresdefault.jpEmber Island  Umbrella.jpg'
         },
         {
             artist: 'AC DC',
@@ -31,17 +61,23 @@ document.addEventListener('DOMContentLoaded', () => {
             imagePath: 'images/AC DC  Hells Bells (Official 4K Video).jpg'
         },
         {
+            artist: 'Banners',
+            songName: 'Someone To You',
+            songPath: 'music/Banners  Someone To You (sub. español).mp3',
+            imagePath: 'images/BANNERS Someone To You (sub. español).png'
+        },
+        {
             artist: 'AC DC',
             songName: 'Let There Be Rock',
             songPath: 'music/AC DC  Let There Be Rock (Apollo Theatre, Glasgow, April 1978).mp3',
             imagePath: 'images/AC DC  Let There Be Rock (Apollo Theatre, Glasgow, April 1978).jpg'
         },
         {
-            artist: 'AC DC',
-            songName: 'Rock or Bust',
-            songPath: 'music/AC DC  Rock or Bust (Official Video).mp3',
-            imagePath: 'images/AC DC  Rock or Bust (Official Video).jpg'
-        },
+            artist: 'Ed Sheeran',
+            songName: 'Perfect',
+            songPath: 'music/Ed Sheeran Perfect (Sub Español).mp3',
+            imagePath: 'images/Ed Sheeran Perfect (Sub Español).png'
+        },       
         {
             artist: 'AC DC',
             songName: 'ROCK',
@@ -49,53 +85,18 @@ document.addEventListener('DOMContentLoaded', () => {
             imagePath: 'images/AC  DC  ROCK.png'
         },
         {
-            artist: 'AC DC',
-            songName: 'You Shook Me All Night Long',
-            songPath: 'music/AC DC  You Shook Me All Night Long (Official 4K Video).mp3',
-            imagePath: 'images/AC DC  You Shook Me All Night Long (Official 4K Video).jpg'
-        },
-        {
             artist: 'Billie Eilish',
             songName: 'Bad Guy',
             songPath: 'music/BAD GUY BILLIE EILISH LETRA EN ESPAÑOL.mp3',
             imagePath: 'images/BAD GUY BILLIE EILISH LETRA EN ESPAÑOL.jpg'
-        },
+        }, 
         {
-            artist: 'Banners',
-            songName: 'Someone To You',
-            songPath: 'music/Banners  Someone To You (sub. español).mp3',
-            imagePath: 'images/BANNERS Someone To You (sub. español).png'
-        },
-        {
-            artist: 'Benson Boone',
-            songName: 'Beautiful Things',
-            songPath: 'music/Benson Boone  Beautiful Things (Español  Lyrics)  video musical.mp3',
-            imagePath: 'images/Benson Boone Beautiful Things.jpg' // Asegúrate de tener esta imagen en la carpeta images
-        },
-        {
-            artist: 'Billie Eilish',
-            songName: 'When The Party’s Over',
-            songPath: 'music/Billie Eilish when the party over (Traducida al Español).mp3',
-            imagePath: 'images/Billie Eilish  when the partys over (Traducida al Español).jpg'
-        },
-        {
-            artist: 'Ed Sheeran',
-            songName: 'Perfect',
-            songPath: 'music/Ed Sheeran Perfect (Sub Español).mp3',
-            imagePath: 'images/Ed Sheeran Perfect (Sub Español).png'
-        },
-        {
-            artist: 'Ember Island',
-            songName: 'Umbrella',
-            songPath: 'music/Ember Island Umbrella (Español).mp3',
-            imagePath: 'images/maxresdefault.jpEmber Island  Umbrella.jpg'
-        },
-        {
-            artist: 'Lewis Capaldi',
-            songName: 'Someone You Loved',
-            songPath: 'music/Someone You Loved  Lewis Capaldi Subtitulada Lyrics.mp3',
-            imagePath: 'images/Someone You Loved  Lewis Capaldi.jpg'
+            artist: 'AC DC',
+            songName: 'You Shook Me All Night Long',
+            songPath: 'music/AC DC  You Shook Me All Night Long (Official 4K Video).mp3',
+            imagePath: 'images/AC DC  You Shook Me All Night Long (Official 4K Video).jpg'
         }
+            
     ];
 
     function loadSongs() {
